10701c75673a96857c9fe33a7de473cc145b1da1
gadzilla143 <egor1231232@gmail.com>
2020-10-01 18:20:17 +0300

    init: arr-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = 0) {
+  if(array == 0) {
+    return 0;
+  }
+  let max = array[0];
+  for (let i = 0; i < array.length; i++){
+    if (array[i] < max) max = array[i];
+  }
+  return max;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = 0) {
+  if(array == 0) {
+    return 0;
+  }
+  let max = array[0];
+  for (let i = 0; i < array.length; i++){
+    if (array[i] > max) max = array[i];
+  }
+  return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = 0) {
+  if(array == 0) {
+    return 0;
+  }
+  let max = 0;
+  for (let i = 0; i < array.length; i++){
+    max += array[i];
+  }
+  return max/array.length;
 }
